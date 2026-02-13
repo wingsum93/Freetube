abcddc7162656f1a4fb33dec68869cb508b63962
Eric Ho <wingsum.developer@gmail.com>
2026-02-13 18:30:08 +0800

    Initial Commit

---
+++ FreeTube/FreeTubeApp.swift
@@ -0,0 +1,32 @@
+//
+//  FreeTubeApp.swift
+//  FreeTube
+//
+//  Created by eric ho on 13/2/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FreeTubeApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ FreeTubeTests/FreeTubeTests.swift
@@ -0,0 +1,17 @@
+//
+//  FreeTubeTests.swift
+//  FreeTubeTests
+//
+//  Created by eric ho on 13/2/2026.
+//
+
+import Testing
+@testable import FreeTube
+
+struct FreeTubeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
